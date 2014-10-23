16efa5b9a0c90e91b5fa83977ca9cfc35e94ab7f
Joe <joe.endor@hotmail.com>
2014-10-23 17:46:17 -0500

    Create main.c
    imagino que no debe ser un programa extraordinario,... lo que pienso es que solo hay que demostrar que hemos hecho uso de esta herramienta... y hemos aprendido su funcionamiento.

---
+++ main.c
@@ -0,0 +1,21 @@
+//codigo para hecer purbas de como funciona github.
+//programa simple que suma, resta, multiplica, divide, muneros ingresador por pantalla.
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    int a,b; 
+    printf ("Primer numero: ");   scanf ("%d",&a); 
+    printf ("Segundo numero: ");  scanf ("%d",&b); 
+    printf ("La suma es: %d",a+b); 
+    
+    
+    //implementar resta, multiplicacion, division
+   
+    
+    return (EXIT_SUCCESS);
+}
+
+
