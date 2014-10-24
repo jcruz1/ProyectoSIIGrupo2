19d3502575184eb1f5985ab11bcbbeb0b562c767
cbarreno <cbarreno@espol.edu.ec>
2014-10-24 06:51:08 -0500

    Implementacion de la funcion multiplicar
    Se implementa la función multiplicar que recibe dos números, los
    multiplica y los imprime

--- main.c
+++ main.c
@@ -4,31 +4,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 void sumar(int a, int b);
 void restar(int a,int b);
+void multiplicar(int a,int b);
 
 int main() {
 
     int a,b; 
 	printf ("Bienvenido... Ingrese 2 numeros!");
-    printf ("Primer numero: ");   scanf ("%d",&a); 
-    printf ("Segundo numero: ");  scanf ("%d",&b); 
+    printf ("Primer numero: ");   
+	scanf ("%d",&a); 
+    printf ("Segundo numero: ");  
+	scanf ("%d",&b); 
+	
     sumar(a,b);
 	restar(a,b);
-    
+    multiplicar(a,b);
     
     //implementar resta, multiplicacion, division
    
     
     return (EXIT_SUCCESS);
 }
+
+void sumar(int a, int b){
+     printf ("La suma es: %d",a+b); 
+}
+
 void restar(int a,int b){
 	printf("La resta es: %d",a-b);
 }
-void sumar(int a, int b){
-     printf ("La suma es: %d",a+b); 
+
+void multiplicar(int a, int b){
+	printf("La multiplicación es: %d", a*b);
 }
 
 
